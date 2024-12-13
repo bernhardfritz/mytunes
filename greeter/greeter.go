0416633898df243ececdce325b416455330b6217
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2024-12-13 22:01:08 +0100

    chore: fix whitespaces

--- greeter/greeter.go
+++ greeter/greeter.go
@@ -1,5 +1,5 @@
 package greeter
 
 func Greet(name string) string {
-	return "Hello " + name
+  return "Hello " + name
 }
--- greeter/greeter_test.go
+++ greeter/greeter_test.go
@@ -3,8 +3,8 @@ package greeter
 import "testing"
 
 func TestGreet(t *testing.T) {
-	want := "Hello Bernhard"
-	if got := Greet("Bernhard"); got != want {
-		t.Errorf("Greet(\"Bernhard\") = %q, want %q", got, want)
-	}
+  want := "Hello Bernhard"
+  if got := Greet("Bernhard"); got != want {
+    t.Errorf("Greet(\"Bernhard\") = %q, want %q", got, want)
+  }
 }
