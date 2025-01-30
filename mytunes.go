7e74ceac1caa7f4fa7c9eca1725aab2b5d4ab393
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2025-01-30 22:06:07 +0100

    feat: add redirect

--- mytunes.go
+++ mytunes.go
@@ -43,15 +43,28 @@ func isSlashRune(r rune) bool {
 	return r == '/' || r == '\\'
 }
 
+// localRedirect gives a Moved Permanently response.
+// It does not convert relative paths to absolute paths like Redirect does.
+func localRedirect(w http.ResponseWriter, r *http.Request, newPath string) {
+	if q := r.URL.RawQuery; q != "" {
+		newPath += "?" + q
+	}
+	w.Header().Set("Location", newPath)
+	w.WriteHeader(http.StatusMovedPermanently)
+}
+
 func interceptor(res http.ResponseWriter, req *http.Request) {
 	if containsDotDot(req.URL.Path) {
 		http.Error(res, "invalid URL path", http.StatusBadRequest)
 		return
 	}
-	// TODO implement redirect from / to /index.m3u => see https://cs.opensource.google/go/go/+/master:src/net/http/fs.go;l=674?q=FileServer&ss=go%2Fgo
-	// also applies to /MasterOfPuppets to /MasterOfPuppets/index.m3u
+
+	if req.URL.Path[len(req.URL.Path)-1] == '/' {
+		req.URL.Path += "index.m3u"
+	}
+
 	if strings.HasSuffix(req.URL.Path, "/index.m3u") {
-		// TODO refactor-extract into dedicated function e.g. servePlaylist
+		// TODO refactor-extract into dedicated function e.g. serveWildcardSlashIndexDotM3u
 		funcMap := template.FuncMap{
 			"PathJoin": path.Join,
 		}
@@ -59,22 +72,24 @@ func interceptor(res http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		dirEntries, err := os.ReadDir(filepath.Join("/var/lib/mytunes", filepath.Dir(req.URL.Path)))
+		dirEntries, err := os.ReadDir(filepath.Join("/var/lib/mytunes", filepath.Dir(req.URL.Path))) // TODO prefer http.Dir("/var/lib/mytunes").Open(filepath.Dir(req.URL.Path)).Readdir(-1) instead
 		if err != nil {
 			log.Fatal(err)
 		}
 		playlist := Playlist{
 			Path:        path.Dir(req.URL.Path),
 			Directories: slices.Collect(itertools.Map(os.DirEntry.Name, itertools.Filter(os.DirEntry.IsDir, slices.Values(dirEntries)))),
-			Files:       slices.Collect(itertools.Map(os.DirEntry.Name, itertools.Filter(itertools.Not(os.DirEntry.IsDir), slices.Values(dirEntries)))),
+			Files:       slices.Collect(itertools.Map(os.DirEntry.Name, itertools.Filter(itertools.Not(os.DirEntry.IsDir), slices.Values(dirEntries)))), // TODO filter files that do not end with .mp3
 		}
 		err = tmpl.Execute(res, playlist)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else if strings.HasSuffix(req.URL.Path, ".m3u8") {
-		// TODO refactor-extract into dedicated function e.g. serveStream
+		// TODO refactor-extract into dedicated function e.g. serveWildcardDotM3u8
 		relativePath := strings.TrimSuffix(req.URL.Path, ".m3u8")
+		// TODO validate that relativePath ends with .mp3
+		// TODO validate that file actually exists
 		input := filepath.Join("/var/lib/mytunes", relativePath)
 		tmpDir := filepath.Join(os.TempDir(), "mytunes")
 		output := filepath.Join(tmpDir, relativePath)
@@ -91,10 +106,33 @@ func interceptor(res http.ResponseWriter, req *http.Request) {
 		log.Println(string(out))
 		http.FileServer(http.Dir(tmpDir)).ServeHTTP(res, req) // TODO http.FileServer(http.Dir("/tmp/mytunes")) can be initialized in main and passed to interceptor as parameter or use the receiver pattern see apiHandler in example of https://pkg.go.dev/net/http#ServeMux.Handle | use os.TempDir() to find the actual dir
 	} else if strings.HasSuffix(req.URL.Path, ".ts") {
-		// TODO refactor-extract into dedicated function e.g. serveSegment
+		// TODO refactor-extract into dedicated function e.g. serveWildcardDotTs
 		tmpDir := filepath.Join(os.TempDir(), "mytunes")
 		http.FileServer(http.Dir(tmpDir)).ServeHTTP(res, req)
 	} else {
+		fs := http.Dir("/var/lib/mytunes")
+		f, err := fs.Open(req.URL.Path)
+		if err != nil {
+			http.NotFound(res, req)
+			return
+		}
+		defer f.Close()
+
+		d, err := f.Stat()
+		if err != nil {
+			http.NotFound(res, req)
+			return
+		}
+
+		if d.IsDir() {
+			url := req.URL.Path
+			// redirect if the directory name doesn't end in a slash
+			if url == "" || url[len(url)-1] != '/' {
+				localRedirect(res, req, path.Base(url)+"/")
+				return
+			}
+		}
+
 		http.NotFound(res, req)
 	}
 }
